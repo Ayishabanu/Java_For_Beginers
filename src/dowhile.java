4d180a53906828d956012b4a4b9d63a55a4b6266
Ayishabanu <71723925+Ayishabanu@users.noreply.github.com>
2022-10-20 17:30:24 +0530

    Create dowhile.java

---
+++ src/dowhile.java
@@ -0,0 +1,15 @@
+package com.dataflair.loops;
+import java.io. * ;
+public class DoWhileLoop {
+  public static void main(String[] args) {
+    int i = 0;
+    do {
+      i++;
+      System.out.println("Learning Java at DataFlair");
+      System.out.println("The value of i is " + i);
+
+    }
+    while ( i != 5 );
+  }
+
+}
